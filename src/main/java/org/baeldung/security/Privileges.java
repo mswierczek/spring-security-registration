29c1d0c0428a90cd4af2b41ac6b40a45b3601d2d
Michal Swierczek <mpswierczek@gmail.com>
2020-02-23 22:20:26 +0100

    - added manager role - created InitialDataService to fix transactions management - moved privileges and roles to constants - created conditional sending emails on unknown device detection - created list of allowed IPs for unknown device - increased version of spring-boot-starter-parent

---
+++ src/main/java/org/baeldung/security/Privileges.java
@@ -0,0 +1,15 @@
+package org.baeldung.security;
+
+public final class Privileges {
+
+    private Privileges() {}
+
+    public static final String CHANGE_PASSWORD_PRIVILEGE = "CHANGE_PASSWORD_PRIVILEGE";
+
+    public static final String MANAGER_PRIVILEGE = "MANAGER_PRIVILEGE";
+
+    public static final String READ_PRIVILEGE = "READ_PRIVILEGE";
+
+    public static final String WRITE_PRIVILEGE = "WRITE_PRIVILEGE";
+
+}
---
+++ src/main/java/org/baeldung/security/Roles.java
@@ -0,0 +1,13 @@
+package org.baeldung.security;
+
+public final class Roles {
+
+    private Roles() {}
+
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public static final String ROLE_MANAGER = "ROLE_MANAGER";
+
+    public static final String ROLE_USER = "ROLE_USER";
+
+}
